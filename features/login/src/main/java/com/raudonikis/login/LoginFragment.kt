efa995b23b3f87c02acd6b50970d2efd49e48982
raudonikis <raudonisernestas@gmail.com>
2021-06-05 13:40:23 +0300

    Set up login viewmodel and viewbinding

--- features/login/src/main/java/com/raudonikis/login/LoginFragment.kt
+++ features/login/src/main/java/com/raudonikis/login/LoginFragment.kt
@@ -1,5 +1,49 @@
 package com.raudonikis.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.raudonikis.login.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginFragment: Fragment(R.layout.fragment_login)
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private val viewModel: LoginViewModel by viewModels()
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    /**
+     * Lifecycle hooks
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /**
+     * Set up
+     */
+    private fun setUpListeners() {
+        binding.buttonSubmit.setOnClickListener {
+            viewModel.onSubmitClicked()
+        }
+    }
+}
