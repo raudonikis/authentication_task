a6902c22c28f358242ad2232e71301c36cdbcd0f
raudonikis <raudonisernestas@gmail.com>
2021-07-14 19:45:36 +0300

    test commit

--- app/src/main/java/com/raudonikis/authentication_task/MainActivity.kt
+++ app/src/main/java/com/raudonikis/authentication_task/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setUpNavigation()
+
     }
 
     private fun setUpNavigation() {
